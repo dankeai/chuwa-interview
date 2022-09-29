a4acefaf31c126a55cfce81b55512cea7a8bdf81
Lule Yan <yanlule93@gmail.com>
2022-09-28 17:17:17 -0700

    Update public class Meeting {.java

--- public class Meeting {.java
+++ public class Meeting {.java
@@ -14,8 +14,21 @@ public class Meeting {
         return true;
     }
     public static void main(String[] args){
-        int[][] meet = {{15,20}, {0,10}};
-        System.out.println(couldAttendAll(meet));
+        int[][] meet1 = {{15,20}, {0,10}};
+        int[][] meet2 = {};
+        int[][] meet3 = {{0,30}, {15,20},{5,10}};
+        int[][] meet4 = {{0,10},{15,20}};
+        int[][] meet5 = {{0,10},{10,20}};
+        int[][] meet6 = {{15,20}, {0,30}};
+        int[][] meet7 = null;
+        System.out.println(couldAttendAll(meet1));
+        System.out.println(couldAttendAll(meet2));
+        System.out.println(couldAttendAll(meet3));
+        System.out.println(couldAttendAll(meet4));
+        System.out.println(couldAttendAll(meet5));
+        System.out.println(couldAttendAll(meet6));
+        System.out.println(couldAttendAll(meet7));
+
     }
     
 }
